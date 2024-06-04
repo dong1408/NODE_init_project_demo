6879300e3f540be12a83aa99f26b575fe872489d
dong1408 <dongden14082002@gmail.com>
2024-06-04 13:15:40 +0700

    add dependence

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config(); 
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/' && req.method === 'GET') {
